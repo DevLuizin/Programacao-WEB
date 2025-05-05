55bce11504571dd444f86c3c8f7c189a14561c7b
Luiz <luizcaetite@gmail.com>
2025-05-05 16:17:43 -0300

    Questão 04

---
+++ Lista de Exercícios 03/questão 04/script.js
@@ -0,0 +1,4 @@
+let x = parseFloat(window.prompt("Digite o primeiro número:"));
+let y = parseFloat(window.prompt("Digite o segundo número:"));
+let diferenca = Math.abs(x - y);
+window.alert("Diferença entre eles: " + diferenca);
