228bf476575b5076962c3833604b785a6ea99637
Luiz <luizcaetite@gmail.com>
2025-05-05 16:16:27 -0300

    Questao 03

---
+++ Lista de Exercícios 03/questão 03/script.js
@@ -0,0 +1,4 @@
+let a = parseFloat(window.prompt("Digite o primeiro número:"));
+let b = parseFloat(window.prompt("Digite o segundo número:"));
+let maior = a > b ? a : b;
+window.alert("Maior número: " + maior);
