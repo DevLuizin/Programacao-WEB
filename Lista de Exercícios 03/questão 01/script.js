f193748fce167e1c218501fb7df914acd183ec79
Luiz <luizcaetite@gmail.com>
2025-05-05 16:13:46 -0300

    Questão 01

---
+++ Lista de Exercícios 03/questão 01/script.js
@@ -0,0 +1,6 @@
+let metros = parseFloat(window.prompt("Informe a distância em metros:"));
+window.alert(
+  "Decímetros: " + (metros * 10) +
+  "\nCentímetros: " + (metros * 100) +
+  "\nMilímetros: " + (metros * 1000)
+);
