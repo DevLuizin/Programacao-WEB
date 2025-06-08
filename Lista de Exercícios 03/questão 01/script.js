f3048401509e8797e90389e1c7ea4057e3e02248
Luiz <luizcaetite@gmail.com>
2025-06-08 11:51:16 -0300

    Refatorar o script de conversão de distância para incluir validação de entrada e melhorar o feedback ao usuário

--- Lista de Exercícios 03/questão 01/script.js
+++ Lista de Exercícios 03/questão 01/script.js
@@ -1,6 +1,17 @@
-let metros = parseFloat(window.prompt("Informe a distância em metros:"));
-window.alert(
-  "Decímetros: " + (metros * 10) +
-  "\nCentímetros: " + (metros * 100) +
-  "\nMilímetros: " + (metros * 1000)
-);
+let metros = parseFloat(prompt("Digite a distância em metros:"));
+
+if (metros =! null) {
+  if (isNaN(metros)) {
+    alert("Por favor, insira um número válido.");
+  } else {
+    let decimetros = metros * 10;
+    let centimetros = metros * 100;
+    let milimetros = metros * 1000;
+
+    alert(`A distância de ${metros} metros é equivalente a:\n` +
+          `${decimetros} decímetros,\n ` +
+          `${centimetros} centímetros e \n` +
+          `${milimetros} milímetros.`
+    );
+  }
+}
