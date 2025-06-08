8799b80b55223fc5b686b987828520e7f009134b
Luiz <luizcaetite@gmail.com>
2025-06-08 11:52:03 -0300

    Remover verificação desnecessária da diferença entre os números

--- Lista de Exercícios 03/questão 04/script.js
+++ Lista de Exercícios 03/questão 04/script.js
@@ -1,4 +1,7 @@
 let x = parseFloat(window.prompt("Digite o primeiro número:"));
 let y = parseFloat(window.prompt("Digite o segundo número:"));
 let diferenca = Math.abs(x - y);
+if (diferenca < 0) {
+    diferenca = diferenca * -1;
+}
 window.alert("Diferença entre eles: " + diferenca);
