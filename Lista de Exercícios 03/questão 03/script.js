cfa8dd6e10f52b93f8cec446f8760f1c53ba65c8
Luiz <luizcaetite@gmail.com>
2025-06-08 11:51:57 -0300

    Adicionar atributo defer ao script e refatorar lógica para comparação de números

--- Lista de Exercícios 03/questão 03/script.js
+++ Lista de Exercícios 03/questão 03/script.js
@@ -1,4 +1,11 @@
 let a = parseFloat(window.prompt("Digite o primeiro número:"));
 let b = parseFloat(window.prompt("Digite o segundo número:"));
-let maior = a > b ? a : b;
+let maior;
+if (a == b) {
+    window.alert("Os números são iguais.");
+} else if (a > b) {
+    maior = a;
+} else {
+    maior = b;
+}
 window.alert("Maior número: " + maior);
