5008b3f75e8d5ac63569f8f74ba51b61f5d95f63
Luiz <luizcaetite@gmail.com>
2025-06-08 11:52:31 -0300

    Atualizar mensagem de alerta para incluir o número digitado pelo usuário

--- Lista de Exercícios 03/questão 05/script.js
+++ Lista de Exercícios 03/questão 05/script.js
@@ -1,3 +1,3 @@
 let numero = parseInt(window.prompt("Digite um número inteiro:"));
 let resultado = numero % 2 === 0 ? "Par" : "Ímpar";
-window.alert("O número é: " + resultado);
+window.alert("O número "+ numero + " é: " + resultado);
