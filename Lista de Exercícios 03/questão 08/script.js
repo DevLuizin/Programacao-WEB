7aace62ab7417bf63bf7d9a89d5a4989fa346976
Luiz <luizcaetite@gmail.com>
2025-06-08 11:52:44 -0300

    Refatorar lógica de cálculo para utilizar switch e adicionar tratamento de divisão por zero

--- Lista de Exercícios 03/questão 08/script.js
+++ Lista de Exercícios 03/questão 08/script.js
@@ -1,12 +1,27 @@
 let valor1 = parseFloat(window.prompt("Digite o primeiro valor:"));
-let valor2 = parseFloat(window.prompt("Digite o segundo valor:"));
 let operador = window.prompt("Digite o operador (+, -, *, /):");
+let valor2 = parseFloat(window.prompt("Digite o segundo valor:"));
 
 let resultado;
-if (operador === "+") resultado = valor1 + valor2;
-else if (operador === "-") resultado = valor1 - valor2;
-else if (operador === "*") resultado = valor1 * valor2;
-else if (operador === "/") resultado = valor1 / valor2;
-else resultado = "Operador inválido";
+switch (operador) {
+    case '+':
+        resultado = val1 + val2;
+        break;
+    case '-':
+        resultado = val1 - val2;
+        break;
+    case '*':
+        resultado = val1 * val2;
+        break;
+    case '/':
+        if (val2 !== 0) {
+        resultado = val1 / val2;
+        } else {
+        resultado = "Divisão por zero é indefinida";
+        }
+        break;
+    default:
+        resultado = "Operador inválido";
+}
 
 window.alert(valor1 + " " + operador + " " + valor2 + " = " + resultado);
