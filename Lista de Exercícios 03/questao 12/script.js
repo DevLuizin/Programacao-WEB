97c9bcb4269bf7ef54e81cc8a11783d3de49a292
Luiz <luizcaetite@gmail.com>
2025-06-08 11:52:56 -0300

    Atualizar lógica para exibir a soma e os divisores de um número inteiro

--- Lista de Exercícios 03/questao 12/script.js
+++ Lista de Exercícios 03/questao 12/script.js
@@ -1,10 +1,11 @@
 let numero = parseInt(window.prompt("Digite um número inteiro:"));
 let soma = 0;
-
+let divisores = [];
 for (let i = 1; i < numero; i++) {
   if (numero % i === 0) {
     soma += i;
+    divisores.push(i);
   }
 }
 
-window.alert("Soma dos divisores de " + numero + " (exceto ele próprio): " + soma);
+window.alert("Soma dos divisores de " + numero + " é (exceto ele próprio): " + divisores + " = " + soma);
