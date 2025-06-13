bae2b8c7f317bc9b8bd7163227579dbbaca57cbf
Luiz <luizcaetite@gmail.com>
2025-06-13 15:34:07 -0300

    Questão 14: Calculadora de Peso Ideal

---
+++ Lista de Exercicios 05/Questão 14/script.js
@@ -0,0 +1,35 @@
+function calcularPesoIdeal() {
+    let sexo = document.getElementById('sexo').value;
+    let alt = document.getElementById('altura').value.trim();
+
+    if (sexo == -1) {
+        alert('Sexo não selecionado!');
+    } else if (alt == '' || isNaN(alt))
+        alert('Altura não informada ou inválida!');
+    else {
+        let peso = sexo == 0 ? 72.7 * parseFloat(alt) - 58 : 62.1 * parseFloat(alt) - 44.7;
+        alert(`Peso Ideal: ${peso.toFixed(2)} kg`);
+
+        let linha = document.createElement('tr');
+
+        let celula = document.createElement('td');
+
+        celula.appendChild(document.createTextNode(sexo == 0 ? 'Masculino' : 'Feminino'));
+        linha.appendChild(celula);
+
+        celula = document.createElement('td');
+        celula.appendChild(document.createTextNode(parseFloat(alt).toFixed(2)));
+        linha.appendChild(celula);
+
+        celula = document.createElement('td');
+        celula.appendChild(document.createTextNode(peso.toFixed(2)));
+        linha.appendChild(celula);
+
+        document.getElementById('tabHistorico').appendChild(linha);
+
+        document.getElementById('tabHistorico').parentNode.style.display = 'flex';
+    }
+}
+
+let botao = document.querySelector('button');
+botao.addEventListener('click', calcularPesoIdeal);
